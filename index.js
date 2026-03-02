e26616ad3ef7f3ddc4b8cbe079b11b14fb1139ed
Shreya Wani <shreyawani2006@gmail.com>
2026-03-02 10:46:51 +0530

    create folder structure and add global error handler and response

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./src/db/index.js";
+import errorHandler from "./src/middlewares/error.middleware.js";
+
+import authRoutes from "./src/routes/auth.routes.js";
+import companyRoutes from "./src/routes/company.routes.js";
+import userRoutes from "./src/routes/user.routes.js";
+import taskRoutes from "./src/routes/task.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/company", companyRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/tasks", taskRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
